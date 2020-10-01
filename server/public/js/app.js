088d281dff5d6110075b5013e39d4503d159caf4
tk1983 <beniwebjp@yahoo.co.jp>
2020-10-02 00:17:30 +0900

    docker6

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -37119,8 +37119,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Program Files\Git\tozan\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Program Files\Git\tozan\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Program Files\Git\tozan\server\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Program Files\Git\tozan\server\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
