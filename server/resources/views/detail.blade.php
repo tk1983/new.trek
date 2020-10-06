3f734debccdd0b70f1af6d97d3815538e24cbb8f
tk1983 <beniwebjp@yahoo.co.jp>
2020-10-06 13:25:52 +0900

    blade2

--- server/resources/views/detail.blade.php
+++ server/resources/views/detail.blade.php
@@ -51,6 +51,14 @@
   transform: translateY(2px);/*下に動く*/
   border-bottom: none;/*線を消す*/
 }
+.hover_set:hover{
+  opacity: 0.6;
+}
+.hover_set:active{
+  -webkit-transform: translateY(10px);
+  transform: translateY(10px);/*下に動く*/
+  border-bottom: none;/*線を消す*/
+}
 h1{
   padding: .25em 0 .25em .75em;
 }
@@ -72,6 +80,68 @@ h1{
   height: 100%;
   background: rgba(255,255,255,0.5);
 }
+.arrow_box{
+    position:relative;
+    width:600px;
+    height:140px;
+    background:#A6E6DB;
+    padding:20px;
+    text-align:left;
+    color:#333333;
+    font-size:20px;
+    font-weight:bold;
+    border-radius:15px;
+    -webkit-border-radius:15px;
+    -moz-border-radius:15px;
+}
+.arrow_box:after{
+    border: solid transparent;
+    content:'';
+    height:0;
+    width:0;
+    pointer-events:none;
+    position:absolute;
+    border-color: rgba(166, 230, 219, 0);
+    border-top-width:10px;
+    border-bottom-width:10px;
+    border-left-width:30px;
+    border-right-width:30px;
+    margin-top: -10px;
+    border-right-color:#A6E6DB;
+    right:100%;
+    top:50%;
+}
+.arrow_box_right{
+    position:relative;
+    width:600px;
+    height:140px;
+    background:#E3E675;
+    padding:20px;
+    text-align:left;
+    color:#333333;
+    font-size:20px;
+    font-weight:bold;
+    border-radius:15px;
+    -webkit-border-radius:15px;
+    -moz-border-radius:15px;
+}
+.arrow_box_right:after{
+    border: solid transparent;
+    content:'';
+    height:0;
+    width:0;
+    pointer-events:none;
+    position:absolute;
+    border-color: rgba(227, 230, 117, 0);
+    border-top-width:10px;
+    border-bottom-width:10px;
+    border-left-width:30px;
+    border-right-width:30px;
+    margin-top: -10px;
+    border-left-color:#E3E675;
+    left:100%;
+    top:50%;
+}
 </style>
 
 @section('content')
@@ -86,14 +156,24 @@ h1{
 </div>
         
 <div class='container'>
-  <p class="text-center"><h1>ユーザーの声</h1></p>
-
+  <p class="center-block"><h1>ユーザーの声</h1></p>
+    <table>
+      <tr><td><img class="face" src="/storage/humanA.png" width="150px" height="150px"></td>
+        <td><div class='arrow_box'>スケジュール調整に役立った。登山口まで行くバスが、12時以降急激に減る事を知ることが出来た。それにより予定より早めに出発し、時間に余裕がもてた。</div></td>
+        <td></td></tr>
+      <tr><td></td>
+        <td><div class='arrow_box_right'>安全に登山することができた。当初予定していた有名な山の登山難度が高く、頂上付近はひどく寒いようだった。自分のレベルにあった山に変更し、楽しく登山を終えることが出来た。</div></td>
+        <td><img class="face" src="/storage/humanB.png" width="150px" height="150px"></td></tr>
+      <tr><td><img class="face" src="/storage/humanC.png" width="150px" height="150px"></td>
+        <td><div class='arrow_box'>登山情報を集める際の時間を短縮できた。様々な情報が載っているので、他の色々なサイトをめぐる必要が無かった。特にコメント欄は投稿者独自の視点からの意見があり、役立った。</div></td>
+        <td></td></tr>
+    </table>
 
   <br><br>
         <form method="GET" action="/detail">
           <select class="btn btn-primary" name="narabi">
-            <option value="asc">日付昇順で並び変え</option>
-            <option value="desc">日付降順で並び変え</option>
+            <option value="asc">更新日付昇順で並び変え</option>
+            <option value="desc">更新日付降順で並び変え</option>
           </select>
           <input class='btn btn-outline-primary' type="submit" value="送信">
         </form>
@@ -112,12 +192,14 @@ h1{
           山の名前：<a href={{ route('detail.detail', ['id' =>  $Trek->id]) }}>
         {{ $Trek->name }}
         </a>
-      　投稿者：{{ $Trek->user->name }}
+      　投稿者：<a href="/users/{{ $Trek->user->id }}">{{ $Trek->user->name }}</a>
         <br>
       @if ($Trek->image_url)
+        <div class='hover_set'>
         <a href={{ route('detail.detail', ['id' =>  $Trek->id]) }}>
         <img src={{ str_replace('public/', 'storage/', $Trek->image_url) }} width="100%" height="auto">
         </a>
+        </div>
       @endif
 
     <div class="card-body">
@@ -146,7 +228,7 @@ h1{
         <div>
           @include('post.comment_list')
         </div>
-          <a class="light-color post-time no-text-decoration" href="/mountain/{{ $Trek->id }}">{{ $Trek->created_at }}</a>
+          <a class="light-color post-time no-text-decoration" href="/mountain/{{ $Trek->id }}">更新：{{ $Trek->updated_at }}</a>
           <hr>
 
             @if (! Auth::check())
